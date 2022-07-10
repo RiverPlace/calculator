5c5359e68727515eed20da647912699d424900a2
River Place <rivspla@gmail.com>
2022-07-09 18:50:25 -0600

    Removed unnecessary semi-colons

--- script.js
+++ script.js
@@ -1,67 +1,68 @@
-let storedValue = '';
-let storedOperation = '';
-let displayedEquation = '';
-let workingValue = '';
+let storedValue = ''
+let storedOperation = ''
+let displayedEquation = ''
+let workingValue = ''
 
-const displayedValue = document.querySelector('.displayed-value');
-const displayedOperation = document.querySelector('.stored-operation');
-const numberButtons = document.querySelectorAll('.num-btn');
-const operatorButtons = document.querySelectorAll('.operator-btn');
-const clearBtn = document.querySelector('.clear-btn');
-const deleteBtn = document.querySelector('.delete-btn');
-const dotBtn = document.querySelector('.dot-btn');
-const equalBtn = document.querySelector('.equal-btn');
+const displayedValue = document.querySelector('.displayed-value')
+const displayedOperation = document.querySelector('.stored-operation')
+const numberButtons = document.querySelectorAll('.num-btn')
+const operatorButtons = document.querySelectorAll('.operator-btn')
+const clearButton = document.querySelector('.clear-btn')
+const deleteButton = document.querySelector('.delete-btn')
+const pointButton = document.querySelector('.dot-btn')
+const equalButton = document.querySelector('.equal-btn')
 
-clearBtn.addEventListener('click', clearCal);
-deleteBtn.addEventListener('click', deleteNum);
-equalBtn.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clearCal)
+deleteButton.addEventListener('click', deleteNum)
+equalButton.addEventListener('click', evaluate)
+pointButton.addEventListener('click', appendPoint)
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const add = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
 
 operatorButtons.forEach(operator => {
     operator.addEventListener('click', (e) => setOperation(e.target.textContent))
 })
 
 numberButtons.forEach(number => {
-    number.addEventListener('click', (e) => displayNumber(e.target.textContent))
+    number.addEventListener('click', (e) => appendNumber(e.target.textContent))
 })
 
-function displayNumber(number) {
+function appendNumber(number) {
     displayedValue.textContent = null;
     workingValue += number;
     displayedValue.textContent = workingValue;
 }
 
 function clearCal() {
-    storedValue = '';
-    storedOperation = '';
-    workingValue = '';
-
-    displayedValue.textContent = 0;
-    displayedOperation.textContent = '';
-};
+    storedValue = ''
+    storedOperation = ''
+    workingValue = ''
+    displayedValue.textContent = 0
+    displayedOperation.textContent = ''
+}
 
 function deleteNum() {
-    let newNum = workingValue.slice(0, -1);
+    let newNum = workingValue.slice(0, -1)
     workingValue = newNum
-    if (workingValue < 1) {workingValue = 0};
+    if (workingValue < 1) {workingValue = 0}
     displayedValue.textContent = workingValue;
-};
+}
+
+function appendPoint() {
+    displayedValue.textContent += '.'
+}
 
 function setOperation(operator) {
-    console.log('Stored: ', storedValue)
-    console.log('Working: ', workingValue)
-
     let properOperator = getOperator(operator)
-    if (storedValue && workingValue) {evaluate()}
-    else if (workingValue !== '') {storedValue = workingValue}
+    if (storedValue && workingValue) evaluate()
+    else if (workingValue !== '') storedValue = workingValue
     storedOperation = properOperator;
-    workingValue = '';
-    displayedOperation.textContent = `${storedValue} ${storedOperation}`;
-};
+    workingValue = ''
+    displayedOperation.textContent = `${storedValue} ${storedOperation}`
+}
 
 function getOperator(symbol) {
     if (symbol === 'x') return '*'
@@ -73,20 +74,21 @@ function getOperator(symbol) {
 function evaluate() {
     let previousNumber = Number(storedValue);
     let currentNumber = Number(workingValue);
-    displayedOperation.textContent = `${storedValue} ${storedOperation} ${workingValue} =`
     let result
+
+    displayedOperation.textContent = `${storedValue} ${storedOperation} ${workingValue} =`
     
     if (storedOperation === '+') {
-        result = add(previousNumber, currentNumber);
+        result = add(previousNumber, currentNumber)
     } else if (storedOperation === '-') {
-        result = subtract(previousNumber, currentNumber);
+        result = subtract(previousNumber, currentNumber)
     } else if (storedOperation === '*') {
-        result = multiply(previousNumber, currentNumber);
+        result = multiply(previousNumber, currentNumber)
     } else if (storedOperation === '/') {
-        result = divide(previousNumber, currentNumber);
+        result = divide(previousNumber, currentNumber)
     }
 
-    displayedValue.textContent = result;
-    storedValue = result;
-    workingValue = '';
-};
+    displayedValue.textContent = result
+    storedValue = result
+    workingValue = ''
+}
